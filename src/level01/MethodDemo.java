330b9bdb9424db3d2466a8075123a2b181105c6d
IU_shi <ayushikk676@gmail.com>
2026-07-26 14:04:02 +0530

    Before IntelliJ project reset

---
+++ src/level01/MethodDemo.java
@@ -0,0 +1,13 @@
+package level01;
+
+public class MethodDemo {
+    public static void displayHeading() {
+        System.out.println("=====================");
+        System.out.println("Student Registration.");
+        System.out.println("=====================");
+    }
+
+    public static void main(String[] args) {
+        MethodDemo.displayHeading();
+    }
+}
